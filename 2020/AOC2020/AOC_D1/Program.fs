5f506537754de210e6e5f8d50298c6f42074ce82
unknown <jean.simon.cormier@outlook.com>
2021-12-01 18:29:53 -0500

    2020.1

---
+++ 2020/AOC2020/AOC_D1/Program.fs
@@ -0,0 +1,55 @@
+﻿open System
+
+printfn "Advent of Code 2020 - Day 1\n"
+
+//Part 1
+let rec fy sum num nums =
+    match nums with
+    | head::tail ->
+        if(num + head = sum) then Some(head)
+        else fy sum num tail
+    | [] -> None
+    
+let rec fx sum nums = 
+    match nums with
+      | [] -> None
+      | head::tail ->
+        let result = fy sum head tail
+        match result with
+          | Some(x) -> Some(head, x)
+          | None -> fx sum tail
+
+printfn "Part 1\n"
+let nums = System.IO.File.ReadLines("../../../input.txt") |> Seq.map int
+let (a, b) = (fx 2020 (List.ofSeq nums)).Value
+let product = a * b
+printfn $"The two numbers are {a} et {b} and their product is {product}.\n"
+
+//Part 2
+let rec gy sum count terms nums =
+    match nums with
+    | head::tail ->
+        let termsSum = List.fold (+) 0 terms
+        if(termsSum + head = sum && terms.Length + 1 = count) 
+            then Some(head::terms)
+        else if(termsSum + head < sum && terms.Length + 1 < count) 
+            then match gy sum count (head::terms) tail with
+                  | Some(x) -> Some(x)
+                  | None -> gy sum count terms tail
+        else gy sum count terms tail
+    | [] -> None
+    
+let rec gx sum count nums = 
+    match nums with
+      | [] -> None
+      | head::tail ->
+        let result = gy sum count [head] tail
+        match result with
+          | Some(x) -> Some(x)
+          | None -> gx sum count tail
+
+printfn "Part 2\n"
+let answers = (gx 2020 3 (List.ofSeq nums)).Value
+let product2 = List.fold (*) 1 answers
+printfn $"The three numbers are {answers} and their product is {product2}.\n"
+     
